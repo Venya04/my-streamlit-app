202d1b95d9eaf65d946987c9d0fcc24b5330491d
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-11 12:10:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -153,9 +153,9 @@ with left_col:
     metrics = compute_metrics(portfolio_returns.dropna())
     st.write(
         pd.DataFrame(metrics, index=["Value"]).T.style.format({
-            "CAGR": "{:.2}",
-            "Volatility": "{:.2}",
-            "Max Drawdown": "{:.2}",
+            "CAGR": "{:.2f}",
+            "Volatility": "{:.2f}",
+            "Max Drawdown": "{:.2f}",
             "Sharpe Ratio": "{:.2f}"
         })
     )
