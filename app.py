a40ec7b141cf0773bc5c25e16e876af7552a14e3
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-11 11:55:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,15 @@ with left_col:
             "Max Drawdown": max_dd
         }
     metrics = compute_metrics(portfolio_returns.dropna())
-    st.write(pd.DataFrame(metrics, index=["Value"]).T.style.format("{:.2%}"))
+  st.write(
+    pd.DataFrame(metrics, index=["Value"]).T.style.format({
+        "CAGR": "{:.2%}",
+        "Volatility": "{:.2%}",
+        "Max Drawdown": "{:.2%}",
+        "Sharpe Ratio": "{:.2f}"
+    })
+)
+
 
     # OUTPERFORMANCE
     sp500_raw = yf.download("SPY", start=START_DATE, end=END_DATE, progress=False)
