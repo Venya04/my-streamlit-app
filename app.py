9492841cbace1a4d535a3483bdc34f0b67d467e1
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-12 15:28:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ color_discrete_map = {
 #         st.markdown("<div class='section-title'>Trader's Conclusion</div>", unsafe_allow_html=True)
 #         conclusion = st.text_area("Summary and suggested action", height=130)
 
-current right col
+# current right col
 with right_col:
     right_box_style = """
         <style>
