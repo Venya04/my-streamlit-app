c75862ad0d8a6dbbf85384b7808eae0e77b4020c
Venya <154803459+Venya04@users.noreply.github.com>
2025-06-12 12:34:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -170,8 +170,8 @@ with left_col:
 
     # === PORTFOLIO HOLDINGS RIGHT UNDER PIE CHART ===
     st.markdown("""
-        <div style='text-align: center; margin-top: 5px;'>
-            <h4 style='margin-bottom: 5px;'>Portfolio Holdings</h4>
+        <div style='text-align: center; margin-top: 10px;'>
+            <h4 style='margin-bottom: 10px;'>Portfolio Holdings</h4>
             <ul style='padding-left: 0; list-style-position: inside; text-align: left; display: inline-block;'>
     """ + "".join([
         f"<li><strong>{asset.capitalize()}</strong>: {weight:.1%}</li>"
